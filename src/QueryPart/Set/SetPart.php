6fccf0abfbbaaa81947ca13131192ce9c9fdf9f1
Alef Castelo <alefaraujocastelo@gmail.com>
2023-08-17 21:16:45 -0600

    adding stringify to set in update

--- src/QueryPart/Set/SetPart.php
+++ src/QueryPart/Set/SetPart.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace MySaasPackage\QueryPart\Set;
 
 use Stringable;
+use MySaasPackage\QueryPart\Stringify;
 
 class SetPart implements Stringable
 {
@@ -16,7 +17,7 @@ class SetPart implements Stringable
     public function __toString(): string
     {
         return 'SET ' . implode(', ', array_map(
-            fn (string $column, string $value) => sprintf('%s = %s', $column, $value),
+            fn (string $column, string $value) => sprintf('%s = %s', $column, Stringify::stringify($value)),
             array_keys($this->values),
             array_values($this->values)
         ));
